768e328cbbb30bf6da2b71cff5a7930dc33150c6
luiz_rattacaso <rattacasoluiz03@gmail.com>
2025-07-26 18:54:31 -0300

    iniciando pesquisa com utilização de API

---
+++ pokemon.py
@@ -0,0 +1,36 @@
+import requests
+base_url = "https://pokeapi.co/api/v2/"
+
+def get_pokemon_info(pokemon):
+    url = f"{base_url}pokemon/{pokemon}"
+    response = requests.get(url)
+
+    try: 
+        if response.status_code == 200:
+            information = response.json()
+            return information
+        else:
+            return print(f"Status code: {response.status_code}")
+
+    except Exception as Error:
+        print("Error: {Error}")
+
+def details():
+    name = input("Enter a pokemon name for better informations: ")
+    try:
+        pokemon_info = get_pokemon_info(name)
+        print(f"Name: {pokemon_info["name"].capitalize()}")
+        print(f"ID: {pokemon_info["id"]}")
+        print(f"weight : {pokemon_info["weight"]}")
+        print(f"height : {pokemon_info["height"]}")
+    except Exception as error:
+        print(f"Something gone wrong!\nErro: {error}")
+    print()
+    choose = input("Do you want to search more pokemon data? (y/n)")
+
+    if "yY" in choose[0]:
+        pass
+    else:
+        return
+
+details()
