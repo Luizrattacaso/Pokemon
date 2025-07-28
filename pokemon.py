40ac340a6d0c06ebb6e4c8efc8ed425c2464ebed
luiz_rattacaso <rattacasoluiz03@gmail.com>
2025-07-28 15:05:26 -0300

    commit na branch evolução

--- pokemon.py
+++ pokemon.py
@@ -19,18 +19,23 @@ def details():
     name = input("Enter a pokemon name for better informations: ")
     try:
         pokemon_info = get_pokemon_info(name)
+        print("-="*30)
         print(f"Name: {pokemon_info["name"].capitalize()}")
         print(f"ID: {pokemon_info["id"]}")
         print(f"weight : {pokemon_info["weight"]}")
         print(f"height : {pokemon_info["height"]}")
-        print()
-        choose = input("Do you want to search more pokemon data? (y/n)")
+        print("-="*30)
+        evolution_url = f"https://pokeapi.co/api/v2/evolution-chain/{pokemon_info["id"]}/"
+        response = requests.get(evolution_url)
 
-        if "Y" in choose[0] or "y" in choose[0]:
-            print("hello")
+        if response.status_code == 200:
+            try:
+                evolution_info = response.text
+                print(evolution_info["EvolutionTrigger"]["names"])
+            except:
+                print("Something gone wrong!")
         else:
-            print("Finishing the program...")
-            print()
+            print(f"Erro status code: {response.status_code}")
     except Exception as error:
         print(f"Something gone wrong!\nErro: {error}")
     
