03e2fdc9b06174fe184899e208c36c64b5825a81
luiz_rattacaso <rattacasoluiz03@gmail.com>
2025-08-12 14:52:45 -0300

    adding a search using Enter key and new start to program

--- main.py
+++ main.py
@@ -5,16 +5,12 @@ from PIL import Image, ImageTk
 from io import BytesIO
 import requests
 
-from utils import get_pokemon_info, description, load_image
-
-#pokemon inicial
-name = "dragonite"
-pokemon_info = get_pokemon_info(name.lower())
+from utils import get_pokemon_info, description, load_image, pokeball_image
 
 co0 = "#09090C"
 co1 = "#feffff"
 co5 = "#ef5350"
-co6 = "#7C5AD2"
+co7 = "#a5e286"
 
 window = Tk()
 window.title("Pokemons")
@@ -34,41 +30,41 @@ ttk.Separator(window, orient= HORIZONTAL).grid(row=0,columnspan=1, ipadx=272)
 style = ttk.Style(window)
 style.theme_use("clam")
 
-frame_pokemon = Frame(window, width= 550, height = 290, relief="flat", background=co6)
+frame_pokemon = Frame(window, width= 550, height = 350, relief="flat", background=co7)
 frame_pokemon.grid(row=1,column=0)
 
 frame_nome = Label(frame_pokemon,
-    text=f"{pokemon_info["name"]}".capitalize(),
+    text="your pokemon".capitalize(),
     relief="flat",
     anchor=CENTER,
     font=("Fixedsys 20 bold"),
-    bg=co6,
+    bg=co7,
     fg=co0)
 frame_nome.place(x=12, y=15)
 
 frame_tipo = Label(frame_pokemon,
-    text=f"{pokemon_info["types"][0]["type"]["name"]}".capitalize(),
+    text=f"type".capitalize(),
     relief="flat",
     anchor=CENTER,
     font=("lvy 10 bold"),
-    bg=co6,
+    bg=co7,
     fg=co0)
 frame_tipo.place(x=12, y=50)
 
 frame_id = Label(frame_pokemon,
-    text=f"#{pokemon_info["id"]}",
+    text=f"#id".capitalize(),
     relief="flat",
     anchor=CENTER,
     font=("lvy 10 bold"),
-    bg=co6,
+    bg=co7,
     fg=co0)
 frame_id.place(x=12, y=75)
 
 description_frame = Label(frame_pokemon,
-    text=f"{description(name)}",
+    text=f"Here will be the description of the pokemon",
     anchor=NW,
     font=("lvy 12"),
-    bg=co6,
+    bg=co7,
     fg=co0,
     justify="left",
     wraplength=150)  #break line
@@ -77,13 +73,10 @@ description_frame.place(x=375, y=100)
 description_frame.lift()
 
 #initial pokemon
-pokemon_image = load_image(name)
+pokeball = pokeball_image()
 
-frame_imagem = Label(frame_pokemon, image=pokemon_image, bg=co6)
-frame_imagem.place(x=90, y=50)
-
-frame_imagem= Label(frame_pokemon,image = pokemon_image, bg=co6,fg=co0)
-frame_imagem.place(x=90, y=50)
+frame_imagem = Label(frame_pokemon, image=pokeball, bg=co7)
+frame_imagem.place(x=180, y=210)
 
 frame_tipo.lift() #o método lift sobrepõe
 
@@ -95,19 +88,19 @@ status_pokemon = Label(window,
     font=("verdana 20 bold"),
     bg=co1,
     fg=co0)
-status_pokemon.place(x=15, y=310)
+status_pokemon.place(x=15, y=360)
 
-pokemon_height = Label(window, text=f"• Height: {pokemon_info["height"]/10} m", relief="flat", anchor=CENTER, font=("lvy 10"),bg=co1,fg=co0)
-pokemon_height.place(x=15, y=365)
+pokemon_height = Label(window, text=f"• Height: m", relief="flat", anchor=CENTER, font=("lvy 10"),bg=co1,fg=co0)
+pokemon_height.place(x=15, y=400)
 
 pokemon_weight = Label(window,
-    text=f"• Weight: {pokemon_info["weight"]/10} Kg",
+    text=f"• Weight: Kg",
     relief="flat",
     anchor=CENTER,
     font=("lvy 10"),
     bg=co1,
     fg=co0)
-pokemon_weight.place(x="15", y="385")
+pokemon_weight.place(x=15, y=425)
 
 #moves
 moves_pokemon = Label(window,
@@ -117,21 +110,7 @@ moves_pokemon = Label(window,
     font=("verdana 20 bold"),
     bg=co1,
     fg=co0)
-moves_pokemon.place(x="285", y="310")
-
-movimentos = pokemon_info['moves'][:5]  #5 moves
-
-for idx, movimento_info in enumerate(movimentos):
-    nome_movimento = movimento_info['move']['name'].replace("-", " ").title()
-    movimento_pokemon = Label(window,
-    text=f"• {nome_movimento}",
-    relief="flat",
-    anchor='w',
-    font=("lvy 10"),
-    bg=co1,
-    fg=co0)
-
-    movimento_pokemon.place(x=290, y=360 + idx * 30)
+moves_pokemon.place(x=285, y=360)
 
 def new_pokemon():
     new_pokemon_name = entry_pokemon.get().strip().lower()
@@ -144,7 +123,7 @@ def new_pokemon():
         "poison": "#B763CD", "ground": "#E2BF65", "rock": "#B6A136",
         "bug": "#A2D97C", "ghost": "#755793", "steel": "#C5CBA3",
         "fire": "#EE8130", "water": "#6390F0", "grass": "#7AC74C",
-        "electric": "#F7D02C", "psychic": "#F95587", "ice": "#96D9D6",
+        "electric": "#ECCE58", "psychic": "#F95587", "ice": "#96D9D6",
         "dragon": "#7C5AD2", "dark": "#705746", "fairy": "#D685AD",
         "stellar": "#6A4C9C", "unknown": "#DFC570"
     }
@@ -176,12 +155,14 @@ def new_pokemon():
         pokemon_weight.config(text=f"• Weight: {new_information['weight']/10} Kg")
 
         # load image
-        nova_imagem = load_image(new_pokemon_name)
-        if nova_imagem:
-            frame_imagem.config(image=nova_imagem, bg=back_ground)
-            frame_imagem.image = nova_imagem  # manter referência
+        new_image = load_image(new_pokemon_name)
+        if new_image:
+            frame_imagem.config(image=new_image, bg=back_ground)
+            frame_imagem.image = new_image  # manter referência
+            frame_imagem.place(x=100, y=80)
         else:
             frame_imagem.config(image=None, text="Image not available", font=("lvy 10"), fg="white", bg=back_ground)
+            frame_imagem.place(x=100, y=80)
 
         # clear moves
         for widget in window.winfo_children():
@@ -189,7 +170,7 @@ def new_pokemon():
                 widget.destroy()
 
         # add new moves
-        new_moves = new_information['moves'][:5]
+        new_moves = new_information['moves'][:3]
         for idx, movimento_info in enumerate(new_moves):
             move_name = movimento_info['move']['name'].replace("-", " ").title()
             pokemon_move = Label(window,
@@ -199,7 +180,7 @@ def new_pokemon():
                 font=("lvy 10"),
                 bg=co1,
                 fg=co0)
-            pokemon_move.place(x=290, y=360 + idx * 30)
+            pokemon_move.place(x=290, y=400 + idx * 30)
 
     except requests.exceptions.ConnectionError:
         description_frame.config(text="❌ Connection error.\nCheck your internet connection.")
@@ -231,6 +212,7 @@ def new_pokemon():
 
 #input
 entry_pokemon = Entry(window, font=("verdana 12"), width=20, bg=co1, fg=co0, relief="solid")
+entry_pokemon.bind('<Return>', lambda event: new_pokemon()) #Enter
 entry_pokemon.place(x=300, y=10)
 
 botao_buscar = Button(window,
@@ -241,7 +223,8 @@ botao_buscar = Button(window,
     fg=co1,
     relief="raised",
     overrelief=RIDGE)
-botao_buscar.place(x=460, y=7)
+
+botao_buscar.place(x=460, y=9)
 
 if __name__ == "__main__":
     window.mainloop()
