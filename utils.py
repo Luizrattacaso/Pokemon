f7b692fa68a27df4b498b1593ffa41bd9ca7d0b1
Luizrattcaso <rattacasoluiz03@gmail.com>
2025-08-31 12:55:09 -0300

    comentário

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageTk
 base_url = "https://pokeapi.co/api/v2/"
 
 def get_pokemon_info(pokemon):
-    url = f"{base_url}pokemon/{pokemon}"
+    url = f"{base_url}pokemon/{pokemon.lower()}/"
     response = requests.get(url)
 
     try:
@@ -32,7 +32,6 @@ def description(pokemon):
                 if entry["language"]["name"] == "en"
             ]
             if english_entries:
-                # Return the first English entry (or join all if you want all texts)
                 return english_entries[0]
         else:
             print(f"Something gone wrong. Status code: {response.status_code}")
@@ -43,7 +42,7 @@ def load_image(name):
     try:
         url = f"http://play.pokemonshowdown.com/sprites/home-centered/{name.lower()}.png"
         response = requests.get(url, timeout=5)
-        response.raise_for_status()
+        response.raise_for_status() #se tiver erro na resposta ele pula para o except
         img_data = BytesIO(response.content)
         img = Image.open(img_data)
         img = img.resize((220, 220), Image.Resampling.LANCZOS)
